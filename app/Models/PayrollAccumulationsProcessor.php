657b719294f013e1e0647f0fcde3943c57d25056
sirrohan <sirrohan1892@gmail.com>
2025-09-17 10:36:04 -0300

    fix: update frequency field retrieval in payroll header query

--- app/Models/PayrollAccumulationsProcessor.php
+++ app/Models/PayrollAccumulationsProcessor.php
@@ -76,7 +76,7 @@ class PayrollAccumulationsProcessor
                     pc.fecha, 
                     pc.tipo_planilla_id,
                     tp.descripcion as tipo_planilla,
-                    tp.frecuencia,
+                    pf.frecuencia_id as frecuencia,
                     MONTH(pc.fecha) as mes,
                     YEAR(pc.fecha) as ano
                 FROM planilla_cabecera pc
